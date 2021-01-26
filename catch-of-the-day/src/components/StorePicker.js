c52deb99a8c5c088b1d1fa326027fe79811e4391
Alex Bang <alexbang@t1cg-ab.lan>
2021-01-26 16:33:49 -0500

    get text from input field

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // get the text from the input
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    console.log("button clicked");
+    // stop page from submitting and refreshing the page
+    event.preventDefault();
+
+    console.log("this-->", this);
+
+    // change the page to /store/what-they-entered
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
