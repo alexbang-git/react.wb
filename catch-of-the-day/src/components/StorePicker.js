386d264bc72b017fde551fd827d9035811d000c7
Alex Bang <alexbang@t1cg-ab.lan>
2021-01-27 08:53:10 -0500

    create property that binds to 'this' to handle form submit

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,22 +2,28 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  // get the text from the input
+  // handleClick() {
+  //   alert("Button clicked!");
+  // }
+  // 2. get the text from the input, create property myInput for the component
   myInput = React.createRef();
 
+  // using custom method like below to handle events won't work becuase it is not bound to 'this'
+  // goToStore(event) {
+  //
+  // instead, create a property for the instance using below syntax to bind it to 'this'
   goToStore = (event) => {
-    console.log("button clicked");
-    // stop page from submitting and refreshing the page
+    // 1. stop page from submitting and refreshing the page
     event.preventDefault();
-
-    console.log("this-->", this);
-
-    // change the page to /store/what-they-entered
+    console.log("this from goToStore property on form submit-->", this);
+    // 3. change the page to /store/what-user-entered
   };
+
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
+        {/* <button onClick={this.handleClick}>Click me!</button> */}
         <input
           type="text"
           ref={this.myInput}
